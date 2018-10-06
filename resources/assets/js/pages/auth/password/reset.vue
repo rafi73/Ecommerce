b7232d7c4ea5a041800374d65196e51745ef1681
Golam Mahmud Rafi <rafi.aust@live.com>
2018-10-07 07:23:42 +0900

    Indentation fixed

--- resources/assets/js/pages/auth/password/reset.vue
+++ resources/assets/js/pages/auth/password/reset.vue
@@ -1,105 +1,84 @@
 <template>
-  <v-layout row>
-    <v-flex xs12 sm8 offset-sm2 lg4 offset-lg4>
-      <v-card>
-        <progress-bar :show="form.busy"></progress-bar>
-        <form @submit.prevent="reset" @keydown="form.onKeydown($event)">
-          <v-card-title primary-title>
-            <h3 class="headline mb-0">{{ $t('reset_password') }}</h3>
-          </v-card-title>
-          <v-card-text>
+	<v-layout row>
+		<v-flex xs12 sm8 offset-sm2 lg4 offset-lg4>
+			<v-card>
+				<progress-bar :show="form.busy"></progress-bar>
+				<form @submit.prevent="reset" @keydown="form.onKeydown($event)">
+					<v-card-title primary-title>
+						<h3 class="headline mb-0">{{ $t('reset_password') }}</h3>
+					</v-card-title>
+					<v-card-text>
 
-            <!-- Email -->
-            <email-input
-              :form="form"
-              :label="$t('email')"
-              :v-errors="errors"
-              :value.sync="form.email"
-              name="email"
-              v-validate="'required|email'"
-            ></email-input>
+						<!-- Email -->
+						<email-input :form="form" :label="$t('email')" :v-errors="errors" :value.sync="form.email" name="email"
+						 v-validate="'required|email'"></email-input>
 
-            <!-- Password -->
-            <password-input
-              :form="form"
-              :hint="$t('password_length_hint')"
-              :v-errors="errors"
-              :value.sync="form.password"
-              v-on:eye="eye = $event"
-              v-validate="'required|min:8'"
-            ></password-input>
+						<!-- Password -->
+						<password-input :form="form" :hint="$t('password_length_hint')" :v-errors="errors" :value.sync="form.password"
+						 v-on:eye="eye = $event" v-validate="'required|min:8'"></password-input>
 
-            <!-- Password Confirmation -->
-            <password-input
-              :form="form"
-              :hide="eye"
-              :label="$t('confirm_password')"
-              :v-errors="errors"
-              :value.sync="form.password_confirmation"
-              data-vv-as="password"
-              hide-icon="true"
-              name="password_confirmation"
-              v-validate="'required|confirmed:password'"
-            ></password-input>
-            
-          </v-card-text>
-          <v-card-actions>
-            <submit-button :flat="true" :form="form" :label="$t('reset_password')"></submit-button>
-          </v-card-actions>
-        </form>
-      </v-card>
-    </v-flex>
-  </v-layout>
+						<!-- Password Confirmation -->
+						<password-input :form="form" :hide="eye" :label="$t('confirm_password')" :v-errors="errors" :value.sync="form.password_confirmation"
+						 data-vv-as="password" hide-icon="true" name="password_confirmation" v-validate="'required|confirmed:password'"></password-input>
+
+					</v-card-text>
+					<v-card-actions>
+						<submit-button :flat="true" :form="form" :label="$t('reset_password')"></submit-button>
+					</v-card-actions>
+				</form>
+			</v-card>
+		</v-flex>
+	</v-layout>
 </template>
 
 <script>
-import Form from 'vform'
+	import Form from 'vform'
 
-export default {
-  name: 'reset-view',
-  
-  metaInfo () {
-    return { title: this.$t('reset_password') }
-  },
-  
-  data: () => ({
-    form: new Form({
-      token: '',
-      email: '',
-      password: '',
-      password_confirmation: ''
-    }),
-    eye: true
-  }),
+	export default {
+		name: 'reset-view',
 
-  methods: {
-    async reset () {
-      if (await this.formHasErrors()) return
+		metaInfo() {
+			return { title: this.$t('reset_password') }
+		},
 
-      this.form.token = this.$route.params.token
+		data: () => ({
+			form: new Form({
+				token: '',
+				email: '',
+				password: '',
+				password_confirmation: ''
+			}),
+			eye: true
+		}),
 
-      const response = await this.form.post('/api/password/reset')
+		methods: {
+			async reset() {
+				if (await this.formHasErrors()) return
 
-      // Login user if reset successful.
-      const { data } = await this.form.post('/api/login')
+				this.form.token = this.$route.params.token
 
-      // Save the token.
-      this.$store.dispatch('saveToken', {
-        token: data.token,
-        remember: false
-      })
+				const response = await this.form.post('/api/password/reset')
 
-      // Fetch the user.
-      await this.$store.dispatch('fetchUser')
+				// Login user if reset successful.
+				const { data } = await this.form.post('/api/login')
 
-      this.$store.dispatch('responseMessage', {
-        type: 'success',
-        text: response.data.status
-      })
+				// Save the token.
+				this.$store.dispatch('saveToken', {
+					token: data.token,
+					remember: false
+				})
 
-      // Redirect home.
-      this.$router.push({ name: 'home' })
-    }
-  }
-}
+				// Fetch the user.
+				await this.$store.dispatch('fetchUser')
+
+				this.$store.dispatch('responseMessage', {
+					type: 'success',
+					text: response.data.status
+				})
+
+				// Redirect home.
+				this.$router.push({ name: 'home' })
+			}
+		}
+	}
 </script>
