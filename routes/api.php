5adebd5afcd2a240666dfc1784cf85af406d5683
Rafi <rafi.aust@live.com>
2018-10-09 13:29:13 +0900

    Working on Auth

--- routes/api.php
+++ routes/api.php
@@ -13,20 +13,44 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('logout', 'Auth\LoginController@logout');
+// Route::group(['middleware' => 'auth:api'], function () {
+//     Route::post('logout', 'Auth\LoginController@logout');
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+//     Route::get('/user', function (Request $request) {
+//         return $request->user();
+//     });
 
-    Route::patch('settings/profile', 'Settings\UpdateProfile');
-    Route::patch('settings/password', 'Settings\UpdatePassword');
+//     Route::patch('settings/profile', 'Settings\UpdateProfile');
+//     Route::patch('settings/password', 'Settings\UpdatePassword');
+// });
+
+// Route::group(['middleware' => 'guest:api'], function () {
+//     Route::post('login', 'Auth\LoginController@login');
+//     Route::post('register', 'Auth\RegisterController@register');
+//     Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+//     Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+// });
+
+
+
+Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::get('logout', 'AuthController@logout');
+    Route::get('refresh', 'AuthController@refresh');
+    Route::get('me', 'AuthController@me');
 });
 
-Route::group(['middleware' => 'guest:api'], function () {
-    Route::post('login', 'Auth\LoginController@login');
-    Route::post('register', 'Auth\RegisterController@register');
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::group(['middleware' => 'jwt.auth'], function ($router) {
+
+    # 1.1 category routes
+    // List category
+    Route::get('categorys', 'CategoryController@index');
+    // List single category
+    Route::get('category/{id}', 'CategoryController@show');
+    // Create new category
+    Route::post('category', 'CategoryController@store');
+    // Update category
+    Route::put('category', 'CategoryController@store');
+    // Delete category
+    Route::delete('category/{id}', 'CategoryController@destroy');
 });
